81320da6e92a0b0d9c610e38fdefc86c89052f25
Anand Gupta <anand.bdk@gmail.com>
2012-05-18 01:04:53 -0700

    added log statements in chunker.py and check for file also

--- python/chunker.py
+++ python/chunker.py
@@ -1,7 +1,7 @@
 !# /usr/bin/exec python
 
 import logging
-import md5,sys
+import md5,sys,os
 from dblayer import *
 
 LOG_FILE_NAME = 'vmdedup.log'
@@ -38,16 +38,26 @@ class chunker:
 		logging.info("chunker:Chunkify: creating chunks for the file :%s", path)
 		try:
 			chunklist = []
+			if(not os.path.exists(path)):
+			  logging.error("chunkify: chunker : invalied file specified")
+			  sys.exit(1)
 			with open(path, 'r') as f:
 				chunk = f.read(40)
 				while("" != chunk):
 					key = self._getmd5(chunk)
+					if(None == Key):
+						 logging.error('chunker:chunkify failed with error : md5  hash was returned as None')
+			                         sys.exit(1)
 					if(db.chunk_exists(key)):
+						logging.info("chunker:chunkify : calling update chunk reference")
 						db.update_chunk_ref(key)
 					else:
+						logging.info("chunker:chunkify : calling add chunk method")
 						db.add_chunk(key,chunk)
 					chunklist.append(key)
-			db.add_file_entry(path, chunklist)					
+			logging.info("chunker:chunkify : calling add file entry method")
+			db.add_file_entry(path, chunklist)			
+			logging.info("chunker:chunkify : successfully completed")		
 			        	
 		except Exception,e:
 			logging.error('chunker:chunkify failed with error : %s', str(e))
@@ -56,8 +66,10 @@ class chunker:
 	def _getmd5(self,chunk):
 		''' returns MD5 of the chunk '''
 		try:
+			logging.info("chunker:_getmd5 method invoked")
 			hasher = md5.new()
 			hasher.update(chunk)
+			logging.info("chunker:_getmd5 successful")
 			return hasher.digest()
 		except Exception,e:
 			logging.error('chunker:_getmd5 : returned error %s',e)
