a0b2f96a5a7c4826f64103fdf045698582f17779
Anand Gupta <anand.bdk@gmail.com>
2012-05-18 13:20:48 -0700

    all the funtions are working now.

--- python/chunker.py
+++ python/chunker.py
@@ -3,15 +3,18 @@
 import logging
 import md5,sys,os
 from dblayer import *
+from datetime import datetime
+
 
 LOG_FILE_NAME = 'vmdedup.log'
+CHUNK_SIZE = 40000
 class chunker:
 	''' This class all the code required for the chunking service. This service will be responsible for splitting of file into chunks and store them in cassandra '''
 	def __init__(self):
 		 '''     1. setup connection to cassandra   2. intialise logger object   '''
 	         try:
 			 global LOG_FILE_NAME
-			 logging.basicConfig(filename =LOG_FILE_NAME, format='%(asctime)s %(message)s')
+			 logging.basicConfig(filename =LOG_FILE_NAME, format='%(asctime)s %(lineno)d %(module)s %(message)s', level =logging.DEBUG)
 			 self.db = dblayer()
 		 except Exception,e:
 			logging.error("chunker:__init__ failed with error %s", e)
@@ -20,6 +23,7 @@ class chunker:
 
 	def chunkify(self,path):
 		'''
+			0.1: check if entry for the file exists in DB
 			1. split the file into chunks	
 			2. calculate MD5 hash of each chunk
 			3. Check if the chunk already exists in the DB
@@ -32,29 +36,46 @@ class chunker:
 				This method assumes that the filename passed is unique and doesn't exists in the DB
 			
 		'''		 	
-		logging.info("chunker:Chunkify: creating chunks for the file :%s", path)
+		logging.info("chunker:Chunkify: creating chunks for the file :%s",path)
 		try:
-			chunklist = []
+			
+			chunklist = []			
 			if(not os.path.exists(path)):
 			  logging.error("chunkify: chunker : invalied file specified")
 			  sys.exit(1)
-			with open(path, 'r') as f:
-				chunk = f.read(40)
+			elif (self.db.file_exists(path)):
+			  logging.error("chunkify: chunker :: an entry for file already exists in db")
+			  sys.exit(1)
+			start_time = datetime.now()
+			file_size = os.path.getsize(path)
+			logging.info("chunker:chunkify :: file shredding initiated for filesize :: %s (bytes) at time: %s", file_size, start_time) 
+		        total_data_written = 0	
+			with open(path, 'rb') as f:
+				chunk = f.read(CHUNK_SIZE)
+                                logging.info("chunker:chunkify :: a chunk of size %s  and type %s was read", str(len(chunk)), type(chunk))
 				while("" != chunk):
 					key = self._getmd5(chunk)
-					if(None == Key):
+					if(None == key):
 						 logging.error('chunker:chunkify failed with error : md5  hash was returned as None')
 			                         sys.exit(1)
-					if(db.chunk_exists(key)):
+					if(self.db.chunk_exists(key)):
 						logging.info("chunker:chunkify : calling update chunk reference")
-						db.update_chunk_ref(key)
+						self.db.update_chunk_ref(key)
 					else:
 						logging.info("chunker:chunkify : calling add chunk method")
-						db.add_chunk(key,chunk)
+						self.db.add_chunk(key,chunk)
+						total_data_written+=40000
 					chunklist.append(key)
+					chunk = f.read(CHUNK_SIZE)
+					logging.info("chunker:chunkify :: a chunk of size %s  and type %s was read", str(len(chunk)), type(chunk))
+
 			logging.info("chunker:chunkify : calling add file entry method")
-			db.add_file_entry(path, chunklist)			
-			logging.info("chunker:chunkify : successfully completed")		
+			self.db.add_file_entry(path, chunklist)			
+			end_time = datetime.now()
+			total_time_taken = end_time - start_time
+			total_time_taken_in_min = (total_time_taken.total_seconds())/60
+			total_space_saved = ((file_size) - total_data_written)
+			logging.info("chunker:chunkify : successfully completed. It took %s minutes. Original Size of file : %s, New size of file : %s, Total space saved : %s bytes", total_time_taken_in_min,file_size, total_data_written, total_space_saved)	
 			        	
 		except Exception,e:
 			logging.error('chunker:chunkify failed with error : %s', str(e))
@@ -67,7 +88,7 @@ class chunker:
 			hasher = md5.new()
 			hasher.update(chunk)
 			logging.info("chunker:_getmd5 successful")
-			return hasher.digest()
+			return hasher.hexdigest()
 		except Exception,e:
 			logging.error('chunker:_getmd5 : returned error %s',e)
 			return None
--- python/dblayer.py
+++ python/dblayer.py
@@ -7,12 +7,12 @@
 # http://pycassa.github.com/pycassa/installation.html
 
 
-import logging
+import logging,sys
 from pycassa.system_manager import *
 from pycassa.pool import ConnectionPool
 from pycassa import ColumnFamily
 LOGFILENAME = 'dblayer.log'
-HOST = 'locahost'
+HOST = 'localhost'
 PORT = '9160'
 KEYSPACE = 'dedup'
 CHUNK_COL_FAMILY = 'chunk'
@@ -21,7 +21,7 @@ FILE_COL_FAMILY = 'files'
 class dblayer:
 	def __init__(self):
 		''' setup a connection to cassandra '''
-		logging.basicConfig(filename = LOGFILENAME, format = '%(asctime)s %(message)s')
+		logging.basicConfig(filename = LOGFILENAME, level = logging.DEBUG, format = '%(asctime)s %(lineno)d %(module)s %(message)s')
 		logging.info("inside dblayer:__init__ method")
 		address = "%s:%s" % (HOST,PORT)
 		try:
@@ -30,7 +30,7 @@ class dblayer:
 		     logging.info("Exiting dblayer:__init__ :  connection to cassandra successful")
 		except Exception, e:
 		     logging.error("Exiting dblayer with error %s" ,str(e))
-
+		     raise e
 
 	def add_chunk(self, key, value):
 		''' method to add chunk to chunks columnfamily 
@@ -41,16 +41,16 @@ class dblayer:
 			This method assumes that the key doesn't exisits in keyspace.
 		  		
                 '''
-		logging.info("dblayer:addchunk : enter with param key as %s", key)
+		logging.info("dblayer:addchunk : enter with param key as %s", str(key))
 		colfamily = ColumnFamily(self.pool,CHUNK_COL_FAMILY)
 		row = key
 		colname = "value"
 		colval = value
 		try:
-			colfamily.insert(row,{colname:value, "ref":0})
+			colfamily.insert(row,{colname:value, "ref":"0"})
 			logging.info("dblayer:addchunk: Chunk successfully added " )
 		except Exception, e:
-			logging.error("exiting dblayer:addchunk with error %s ",e)
+			logging.error("exiting dblayer:addchunk with error %s ", str(e))
 		
 
 	def add_file_entry(self, filename, chunklist):
@@ -58,38 +58,59 @@ class dblayer:
 		logging.info("dblayer:addfileentry  : entered with filename as %s" , filename)
 		colfamily = ColumnFamily(self.pool, FILE_COL_FAMILY)
 		chunknumbers = range(0, len(chunklist))
+		chunkstr = list(map(str,chunknumbers))
+		entries =  dict(zip(chunkstr, chunklist))
+		logging.info("totalchunks = %s and totalchunklist = %s and entries = %s", len(chunkstr), len(chunklist), len(entries))
 	        try:
-			colfamily.insert(filename, {zip(chunknumbers, chunklist)})
+			colfamily.insert(filename, entries)
 			logging.info("dblayer:addfileentry : entry for file %s created ", filename)
-		except Exception,e :
+		except Exception,e:
 			logging.error("dblayer:addfileentry has errors %s ",str(e))
-		
+			sys.exit(1)	
 
 
 	def chunk_exists(self, key):
 		''' chekcs if key exisits in the Chunk keyspace and returns true/false '''
-		logging.info("inside dblayer:chunkexists to check chunk %", key)
+		logging.info("inside dblayer:chunkexists to check chunk %s", str(key))
 		try:
 		    colfamily = ColumnFamily(self.pool, CHUNK_COL_FAMILY)
 		    chunk = colfamily.get(key)
-		    if null != chunk:
+		    if None != chunk:
 			return True
 		    else:
 			return False
 		except Exception,e:
-		   logging.error("dblayer:chunkexists has failed with error %s", str(e))
-		   throw(e)
+		   return False
+	
+	def file_exists(self,key):
+		''' checks if an entry for file alread exists in DB '''
+		logging.info("inside dblayer:file_exists method	with filename = %s", key)	
+		try:
+                    colfamily = ColumnFamily(self.pool, FILE_COL_FAMILY)
+                    chunk = colfamily.get(key)
+                    if None != chunk:
+                        return True
+                    else:
+                        return False
+                except Exception,e:
+                   return False
 
 
-	def update_chunk_ref(self, key):
+	def update_chunk_ref(self, key,value=1):
 		 ''' method to update the reference count of a exisitng chunk 
 		   1. fetch the chunk
 		   2. increment the refcount entry by 1
 		   3. update the entry
 		 '''
-		 colfamily = ColumnFamily(self.pool, CHUNK_COL_FAMILY)
-                 chunk = colfamily.get(key)
-                 chunk['ref'] = str(int(chunk['ref']+1))
-                 colfamily.insert(key,chunk)
+		 try:
+		 	logging.info("dblayer:update_chunk_ref invoked for key : %s", key)
+			colfamily = ColumnFamily(self.pool, CHUNK_COL_FAMILY)
+	                chunk = colfamily.get(key)
+        	        chunk['ref'] = str(int(chunk['ref'])+value)
+                	colfamily.insert(key,chunk)
+			logging.info("dblayer:update_chunk_ref successful")
+		 except Exception,e:
+			logging.error("dblayer:update_chunk_ref failed with error : %s", str(e))
+			raise e
 
 	
