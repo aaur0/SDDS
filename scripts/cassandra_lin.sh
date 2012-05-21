f37ee6c5e17b4765981df76fa092fc4d65308946
Anand Gupta <anand.bdk@gmail.com>
2012-05-20 23:04:48 -0700

    added script to automate installation of cassandra on linux

---
+++ scripts/cassandra_lin.sh
@@ -0,0 +1,19 @@
+# script to install cassandra on any debian derivative
+
+
+sudo -s
+echo "#source for installing on  cassandra" >> /etc/apt/sources.list
+echo "deb http://www.apache.org/dist/cassandra/debian 11x main" >> /etc/apt/sources.list
+echo "deb-src http://www.apache.org/dist/cassandra/debian 11x main" >> /etc/apt/sources.list
+
+
+gpg --keyserver pgp.mit.edu --recv-keys F758CE318D77295D
+gpg --export --armor F758CE318D77295D | sudo apt-key add -
+
+gpg --keyserver pgp.mit.edu --recv-keys 2B5C1B00
+gpg --export --armor 2B5C1B00 | sudo apt-key add -
+
+sudo apt-get update
+sudo apt-get install cassandra
+
+
