dae9387d460fb5fb3a6f4113e3f69705f4a4f7fb
Prakash Chandrasekaran <techianprakash@gmail.com>
2012-05-27 16:28:51 -0700

    removed few comments

--- python/dblayer.py
+++ python/dblayer.py
@@ -6,15 +6,7 @@
 # For more inforamtion visit
 # http://pycassa.github.com/pycassa/installation.html
 
-#create keyspace minhash;
-#use minhash;
-#create column family chunks; # list of merged chunk ids and their data
-#create column family filerecipe; # list of file names and their chunk ids
-#create column family fullhash; # list of full hashes
-
-#create keyspace files; 
-#use files;
-#create column family minhash; # one to one mapping of files and its minhash
+# create keyspace and column families
 
 import logging,sys
 from pycassa.system_manager import *
